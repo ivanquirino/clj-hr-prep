19cc3ea83d9bfd04d1954e3c3856336d4de66fff
Ivan Quirino <ivanquirino1928@gmail.com>
2023-01-17 15:31:14 -0300

    left rotation fix output

--- src/clj_hr_prep/left_rotation.clj
+++ src/clj_hr_prep/left_rotation.clj
@@ -1,5 +1,4 @@
-(ns clj-hr-prep.left-rotation
-  (:require [clojure.string :as str]))
+(ns clj-hr-prep.left-rotation)
 
 (def input1 {:a [1 2 3 4 5] :d 4 })
 (def input2 {:a [41 73 89 7 10 1 59 58 84 77 77 97 58 1 86 58 26 10 86 51] :d 10})
@@ -8,8 +7,12 @@
 (defn rotateLeftOnce [a]
   (conj (vec (rest a)) (first a)))
 
+
+; HackerRank asks to return the updated array to be printed as a single line of space-separated integers,
+; but if we do that, it fails. Just returning the array works.
 (defn rotLeft [a d]
   (loop [i 0 v a]
     (if (>= i d)
-      (str/join " " v)
+      v
       (recur (inc i) (rotateLeftOnce v)))))
+
