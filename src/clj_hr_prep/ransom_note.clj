b03b50ed955dcd0dc79307be0a73ad1dcf01ff4f
Ivan Quirino <ivanquirino1928@gmail.com>
2023-01-21 14:34:48 -0300

    ransom note tests

--- src/clj_hr_prep/ransom_note.clj
+++ src/clj_hr_prep/ransom_note.clj
@@ -1,12 +1,12 @@
 (ns clj-hr-prep.ransom-note)
 
-(def input0 { :m ["give" "me" "one" "grand" "today" "night"]
-             :n ["give" "one" "grand" "today"] })
+(def input0 {:m ["give" "me" "one" "grand" "today" "night"]
+             :n ["give" "one" "grand" "today"]})
 
-(def input1 { :m ["ive" "got" "a" "lovely" "bunch" "of" "coconuts"]
-             :n ["ive" "got" "some" "coconuts"] })
+(def input1 {:m ["ive" "got" "a" "lovely" "bunch" "of" "coconuts"]
+             :n ["ive" "got" "some" "coconuts"]})
 
-(def input2 { :m ["two" "times" "three" "is" "not" "four"]
+(def input2 {:m ["two" "times" "three" "is" "not" "four"]
              :n ["two" "times" "two" "is" "four"]})
 
 (defn checkMagazineBoolean [magazine note]
@@ -14,20 +14,21 @@
                      (fn [m word]
                        (assoc! m word (+ 1 (get m word 0))))
                      (transient {})
-                     magazine)]
-    (reduce
-      (fn [[result m] word]
-        (let [value (get mWordCount word 0)
-              found (> value 0)]
-          (if (not found)
-            (reduced [false m])
-            [result (assoc! m word (dec (get m word 0)))])))
-      [true mWordCount]
-      note)))
+                     magazine)
+        result (reduce
+                 (fn [[result m] word]
+                   (let [value (get mWordCount word 0)
+                         found (> value 0)]
+                     (if (not found)
+                       (reduced [false m])
+                       [result (assoc! m word (dec (get m word 0)))])))
+                 [true mWordCount]
+                 note)]
+    (get result 0)))
 
 (defn checkMagazine [magazine note]
   (println
-    (if (get (checkMagazineBoolean magazine note) 0)
+    (if (checkMagazineBoolean magazine note)
       "Yes"
       "No")))
 
---
+++ test/clj_hr_prep/ransom_note_test.clj
@@ -0,0 +1,9 @@
+(ns clj-hr-prep.ransom_note_test
+  (:require [clojure.test :refer :all]
+            [clj-hr-prep.ransom-note :refer :all]))
+
+(deftest ransomNoteTest
+  (let [actual (for [input [input0 input1 input2] :let [{m :m n :n} input]] (checkMagazineBoolean m n))
+        expected [true false false]]
+    (println actual)
+    (is (= actual expected))))
