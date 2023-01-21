8fac4a8b60c8462397e6d46914bf20280650e740
Ivan Quirino <ivanquirino1928@gmail.com>
2023-01-21 00:47:39 -0300

    ramson note: reduced solution

--- src/clj_hr_prep/ransom_note.clj
+++ src/clj_hr_prep/ransom_note.clj
@@ -15,17 +15,19 @@
                        (assoc! m word (+ 1 (get m word 0))))
                      (transient {})
                      magazine)]
-    (loop [n note m mWordCount]
-      (let [key (first n) value (get m key 0) rNote (rest n) fnd (> value 0)]
-        (if (empty? n)
-          true
-          (if (not fnd)
-            false
-            (recur rNote (assoc! m key (- value 1)))))))))
+    (reduce
+      (fn [[result m] word]
+        (let [value (get mWordCount word 0)
+              found (> value 0)]
+          (if (not found)
+            (reduced [false m])
+            [result (assoc! m word (dec (get m word 0)))])))
+      [true mWordCount]
+      note)))
 
 (defn checkMagazine [magazine note]
   (println
-    (if (checkMagazineBoolean magazine note)
-    "Yes"
-    "No")))
+    (if (get (checkMagazineBoolean magazine note) 0)
+      "Yes"
+      "No")))
 
