9c4e8d4c1dfe99790b897a8ec4844f5f52e91bff
Ivan Quirino <ivanquirino1928@gmail.com>
2023-01-23 13:23:43 -0300

    remove native-image

--- project.clj
+++ project.clj
@@ -7,17 +7,4 @@
   :main ^:skip-aot clj-hr-prep.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
-             :native-image ;; used by default
-             {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]
-              :opts ["--report-unsupported-elements-at-runtime"
-                     "--initialize-at-build-time"
-                     "--verbose"]}
-             :test    ;; e.g. lein with-profile +test native-image
-             {:native-image {:opts ["--report-unsupported-elements-at-runtime"
-                                    "--initialize-at-build-time"
-                                    "--verbose"]}}}
-  :plugins [[io.taylorwood/lein-native-image "0.3.1"]]
-  :native-image {:name "clj-hr-prep"                 ;; name of output image, optional
-                 :graal-bin "/home/ivan/.jdks/graalvm-ce-19" ;; path to GraalVM home, optional
-                 :opts ["--verbose"]})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
